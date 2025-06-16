2f273c32e1c8e8db089c2bae1b8063edc6090569
Sarah Mendes <sarahmendesandrade1@gmail.com>
2025-06-16 20:07:17 -0300

    Delete apitechrep directory

--- apitechrep/src/main/java/com/techrep/apitechrep/repository/ClientesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.techrep.apitechrep.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.techrep.apitechrep.entity.ClientesEntity;
-
-public interface ClientesRepository extends JpaRepository<ClientesEntity, Integer> {
-
-}
